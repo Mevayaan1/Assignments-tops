f28aa27a3f73d66fe5e5fcf8d580e0de56e26216
AYAAN MEV <135454841+Mevayaan1@users.noreply.github.com>
2024-03-24 13:01:32 +0530

    Add files via upload

---
+++ JavaScript-Essentials-And-Advanced/work.js
@@ -0,0 +1,28 @@
+function sub(){
+    let   time = document.getElementById("one")
+    
+    if(time.type == "password"){
+      time.type = "text";
+
+    }
+    else{
+
+        time.type ="password";
+
+      }
+}
+
+function icon(){
+    let   times = document.getElementById("one-1")
+    
+    if(times.type == "password"){
+      times.type = "text";
+
+    }
+     
+    else{
+
+        times.type ="password";
+
+      }
+} 
